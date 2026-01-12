b80e8b5f5044949c8a40138be94e33b6c1d2debe
fyrmuen <fyrmuenn@gmail.com>
2026-01-12 20:53:41 +0700

    latihan awal

---
+++ latihan1/angkot.js
@@ -0,0 +1,9 @@
+let jmlAngkot = 10;
+let noAngkot = 1;
+
+alert("Lakukan pengecekan angkot dari nomor 1 sampai " + jmlAngkot);
+
+while (noAngkot <= jmlAngkot) {
+  console.log("Angkot No. " + noAngkot + " beroperasi dengan baik.");
+  noAngkot++;
+}
